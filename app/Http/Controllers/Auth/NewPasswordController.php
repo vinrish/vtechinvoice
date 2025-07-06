b66ce583508aa1c5e2fa99c9e63e9919f263b12c
Vincent Kariuki <31731244+vinrish@users.noreply.github.com>
2025-07-06 22:17:40 +0300

    refactor: Refactored NewPasswordController

--- app/Http/Controllers/Auth/NewPasswordController.php
+++ app/Http/Controllers/Auth/NewPasswordController.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Auth\Events\PasswordReset;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
